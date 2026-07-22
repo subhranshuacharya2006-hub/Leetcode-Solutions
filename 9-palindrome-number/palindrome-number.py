1ff53a7a9fd99e58ffa206d20535e38fa78e8874
Sierra Alpha <subhranshuacharya2006@gmail.com>
2026-07-22 23:07:59 +0530

    Time: 7 ms (78.07%) | Memory: 12.4 MB (54.30%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        s = str(x)
+
+        if s == s[::-1]:
+            return True
+        else:
+            return False
+            
+        
