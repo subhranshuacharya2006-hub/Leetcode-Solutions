c137b6637decfbd153c614744ce27f3366b5fe0d
Sierra Alpha <subhranshuacharya2006@gmail.com>
2026-08-14 06:18:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (90.22%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,10 +1,14 @@
-class Solution(object):
+class Solution:
     def isPalindrome(self, x):
-        s = str(x)
-
-        if s == s[::-1]:
-            return True
-        else:
+        if x < 0:
             return False
-            
-        
+
+        original = x
+        reverse = 0
+
+        while x != 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x = x // 10
+
+        return original == reverse
